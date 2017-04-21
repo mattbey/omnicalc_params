77b32459d617ee605da03b2be6f3c4bcd3c25a68
mattbey <matt.bey@gmail.com>
2017-04-20 22:42:37 -0500

    4/20 work

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,61 @@
+class CalculationsController < ApplicationController
+
+  def flexible_square_5
+    # Parameters: {"number"=>"8"}
+    @user_num = params["number"].to_f
+    render("calculations/flexible_square_5.html.erb")
+  end
+  def flexible_square_root
+    # Parameters: {"number"=>"8"}
+    rawdata = params["number"].to_f
+    @user_num = rawdata
+    render("calculations/flexible_square_root.html.erb")
+  end
+
+  def flexible_payment
+    # Parameters: {"number"=>"8"}
+    @apr = params["apr"].to_f
+    @years = params["years"].to_i
+    @principal = params["principal"].to_f
+    @monthly_payment = ((((@apr/10000)/12) * @principal) /(1 - (1 + ((@apr/10000)/12)) ** ( -(@years * 12))))
+    render("calculations/flexible_payment.html.erb")
+  end
+  def flexible_random
+    # Parameters: {"number"=>"8"}
+    @number = 50
+    @number2 = 100
+    @random_number = rand(50..100)
+    render("calculations/flexible_random.html.erb")
+  end
+  def square_form
+    # Parameters: {"number"=>"8"}
+    render("calculations/square_form.html.erb")
+  end
+  def square
+    # Parameters: {"number"=>"8"}
+    # Parameters: {"user_num"=>"44"}
+    @user_num = params[:user_number].to_f
+    render("calculations/square.html.erb")
+  end
+  def square_root_form
+    # Parameters: {"number"=>"8"}
+    render("calculations/square_root_form.html.erb")
+  end
+  def square_root
+    # Parameters: {"number"=>"8"}
+    # Parameters: {"user_num"=>"44"}
+    @user_num = params[:user_number].to_f
+    render("calculations/square_root.html.erb")
+  end
+  def payment_form
+    # Parameters: {"number"=>"8"}
+    render("calculations/payment_form.html.erb")
+  end
+  def payment
+    # Parameters: {"number"=>"8"}
+    # Parameters: {"user_num"=>"44"}
+    @user_num = params[:user_number].to_f
+    render("calculations/payment.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get("/flexible/square/:number", {:controller => "calculations", :action => "flexible_square_5"})
+get("/flexible/square_root/:number", {:controller => "calculations", :action => "flexible_square_root"})
+get("/flexible/payment/:apr/:years/:principal", {:controller => "calculations", :action => "flexible_payment"})
+get("/flexible/random/:number/:number2", {:controller => "calculations", :action => "flexible_random"})
+get("/square/new", {:controller => "calculations", :action => "square_form"})
+get("/square/results", {:controller => "calculations", :action => "square"})
+get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+get("/square_root/results", {:controller => "calculations", :action => "square_root"})
+get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+get("/payment/results", {:controller => "calculations", :action => "payment"})
+
 end
